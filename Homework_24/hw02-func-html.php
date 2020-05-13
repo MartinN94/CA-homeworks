a09f1f06a3bd140c9b88a05a2e4a5d8599455b8a
MartinN94 <nikolov.m@hotmail.com>
2020-05-13 20:16:33 +0200

    HW_24

---
+++ Homework_24/hw02-func-html.php
@@ -0,0 +1,73 @@
+<?php 
+
+    const months = [
+        "January" => "31",
+        "February" => "28 days, if leap year 29",
+        "March" => "31",
+        "April" => "30",
+        "May" => "31",
+        "June" => "30",
+        "July" => "31",
+        "August" => "31",
+        "September" => "30",
+        "October" => "31",
+        "November" => "30",
+        "December" => "31"
+    ];
+    
+    function makeOptions(){
+        foreach (months as $key => $value) {
+            echo "<option value='$key'>$key</option>";
+        }
+    };
+
+    function makeSelect(){
+        if (months) {
+            echo "<select name='months' id='months'>";
+            echo "<option selected>Choose month</option>";
+            echo makeOptions();
+            echo "</select>";
+        } else {
+            echo "<script> alert('Select valid months array!') </script>";
+        }
+        
+    };
+
+    function response(){
+        if (isset($_POST['submitt'])) {
+            $selected = $_POST["months"];
+        };
+        if (isset($selected)) {
+            echo $selected." has: ".months[$selected]." days";
+        };
+    };
+
+
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="POST">
+        <label for="months"><b>Please choose a month.</b></label>
+        <br>
+        <br>
+        <?php
+            makeSelect();
+        ?>
+        <button type="submit" name="submitt">Submit</button>
+    </form>
+    <p>
+        <?php
+            response();
+        ?>
+    </p>
+
+</body>
+</html>
