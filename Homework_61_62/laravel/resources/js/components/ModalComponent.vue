69d00a07addb1df1f000035e7f63d383fb85a91f
Martin Nikolov <nikolov.m@hotmail.com>
2020-09-03 16:36:09 +0200

    HW_61&62

---
+++ Homework_61_62/laravel/resources/js/components/ModalComponent.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="container">
+    <!-- Modal -->
+    <div
+      class="modal fade"
+      id="exampleModalCenter"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="exampleModalCenterTitle"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLongTitle">
+              <slot name="title"></slot>
+            </h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <p>
+              <slot name="body"></slot>
+            </p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">
+              <slot name="close"></slot>
+            </button>
+            <button type="button" class="btn btn-primary">
+              <slot name="save"></slot>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <button
+      type="button"
+      class="btn btn-primary"
+      data-toggle="modal"
+      data-target="#exampleModalCenter"
+    >
+      <slot name="toggle"></slot>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
